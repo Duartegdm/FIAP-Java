4b84f7861ee30438b827fe4822dc410c3112bcdb
Rodrigo <labsfiap@fiap.com.br>
2025-05-12 22:44:26 -0300

    Aula 8 e 9

---
+++ src/aula09/br/com/fiap/credito/view/Main.java
@@ -0,0 +1,22 @@
+package aula09.br.com.fiap.credito.view;
+
+import aula09.br.com.fiap.credito.model.Endereco;
+import aula09.br.com.fiap.credito.model.Pessoa;
+import aula09.br.com.fiap.credito.model.PessoaFisica;
+import aula09.br.com.fiap.credito.model.PessoaJuridica;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        Pessoa pessoa = new Pessoa();
+        PessoaFisica pessoaFisica = new PessoaFisica();
+        PessoaJuridica pessoaJuridica = new PessoaJuridica();
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+}
